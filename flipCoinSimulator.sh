58b7dfff540c24d1330085a575da3905fed70fff
ekta12345 <ektakumbhare3@gmail.com>
2021-03-21 10:28:56 +0530

    Added: Code for number of times either heads or tails win

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
-Heads=1
-randomNumber=$(( RANDOM%2 ))
-if [ $randomNumber -eq $Heads ]
-then
-   echo "Heads is the Winner"
-else
-   echo "Tails is the Winner"
-fi
+#!/bin/bash -x
+heads=1
+tails=1
+no_of_cointoss=10
+for (( i=1 ; i<=10 ; i++ ))
+do
+	random=$(( $RANDOM%2 ))
+	if (( $random == 1 ))
+	then
+	echo "heads"
+		heads=$(( $heads + 1 ))
+	else
+	echo "tails"
+		tails=$(( $tails +1 ))
+	fi
+done
+echo "Number of times heads wins:" $heads
+echo "Number of times tails wins:" $tails
